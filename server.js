4967bedf9b1a28fcd90450b9d4a3ea8e0e993025
pascalspencer <pascalouma74480@gmail.com>
2024-11-04 09:52:21 +0300

    permission

--- server.js
+++ server.js
@@ -4,12 +4,12 @@ const fileupload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const admin = require('firebase-admin');
-const serviceAccount = require('./bloggingSiteKey.json'); // Your Firebase service account key
+// const serviceAccount = require('./bloggingSiteKey.json'); // Your Firebase service account key
 
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
-    storageBucket: 'blogging-site-4e017.appspot.com'
-});
+// admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount),
+//     storageBucket: 'blogging-site-4e017.appspot.com'
+// });
 
 // const bucket = admin.storage().bucket();
 
